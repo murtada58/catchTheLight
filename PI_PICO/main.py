26024c02ad8897eb63c24262ec04c1cbe53153db
Murtada <murtada58@hotmail.com>
2025-03-05 23:51:52 +0000

    refactor and improve

--- PI_PICO/main.py
+++ PI_PICO/main.py
@@ -2,11 +2,12 @@ from machine import I2C, Pin
 from time import time, sleep
 from random import choice
 from pico_i2c_lcd import I2cLcd
-from utils import getRandomInt, getRandomIntArray, manageLights
+from utils import getRandomInt, getRandomIntArray, updateLeds, resetPlayers, displayPlayerScores, displayAndReturnWinners, turnOffAllLeds, turnOnAllPlayerLeds
 
 NUM_SELECTED = 2
-COUNTDOWN_TIME = 3
-GAME_LENGTH = 30
+COUNTDOWN_DURATION = 3
+GAME_DURATION = 30
+WINNER_FLASH_DURATION = 1
 
 i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 I2C_ADDR = i2c.scan()[0]
@@ -14,113 +15,100 @@ lcd = I2cLcd(i2c, I2C_ADDR, 4, 20)
 
 START_BUTTON = Pin(2, Pin.IN, Pin.PULL_DOWN)
 
-# TODO commonise to X number of players using a dict
-
-# PLAYER 1
-LEDS = [
-    Pin(16, Pin.OUT),
-    Pin(17, Pin.OUT),
-    Pin(18, Pin.OUT)
+PLAYERS = [
+    {
+        "NAME": "P1",
+        "LEDS": [
+            Pin(16, Pin.OUT),
+            Pin(17, Pin.OUT),
+            Pin(18, Pin.OUT)
+        ],
+        "BUTTONS": [
+            Pin(15, Pin.IN, Pin.PULL_DOWN),
+            Pin(14, Pin.IN, Pin.PULL_DOWN),
+            Pin(13, Pin.IN, Pin.PULL_DOWN)
+        ],
+        "SCORE": 0,
+        "SELECETD": []
+    },
+    {
+        "NAME": "P2",
+        "LEDS": [
+            Pin(6, Pin.OUT),
+            Pin(7, Pin.OUT),
+            Pin(8, Pin.OUT)
+        ],
+        "BUTTONS": [
+            Pin(3, Pin.IN, Pin.PULL_DOWN),
+            Pin(4, Pin.IN, Pin.PULL_DOWN),
+            Pin(5, Pin.IN, Pin.PULL_DOWN)
+        ],
+        "SCORE": 0,
+        "SELECETD": []
+    },
 ]
-BUTTONS = [
-    Pin(15, Pin.IN, Pin.PULL_DOWN),
-    Pin(14, Pin.IN, Pin.PULL_DOWN),
-    Pin(13, Pin.IN, Pin.PULL_DOWN)
-]
 
-# PLAYER 2
-LEDS2 = [
-    Pin(6, Pin.OUT),
-    Pin(7, Pin.OUT),
-    Pin(8, Pin.OUT)
-]
-BUTTONS2 = [
-    Pin(3, Pin.IN, Pin.PULL_DOWN),
-    Pin(4, Pin.IN, Pin.PULL_DOWN),
-    Pin(5, Pin.IN, Pin.PULL_DOWN)
-]
-
-
-lcd.putstr("PRESS START BUTTON\nTO PLAY")
-
-score = 0
-score2 = 0
-
-# turns off all the LEDS
-manageLights(LEDS, [])
-manageLights(LEDS2, [])
+lcd.putstr("{:^20}".format("PRESS START BUTTON"))
+lcd.putstr("{:^20}".format("TO PLAY"))
+resetPlayers(PLAYERS, NUM_SELECTED)
 
 started = False
 start_time = 0
 next_update_time = 0
+score_needs_update = False
+winners = None
+next_flash_time = 0
+is_flash_on = False
 
+# Currently too slow sometimes inputs are being missed!!!
 while True:
+    curr_time = time()
     if started:
-        startScore = score
-        startScore2 = score2
-        manageLights(LEDS, selected)
-        manageLights(LEDS2, selected2)
-        curr_time = time()
-        for i, num in enumerate(selected):
-            if BUTTONS[num].value():
-                score += 1
-                selected[i] = getRandomInt(0, len(LEDS), selected)
-                print(score)
-                # sleep(0.1) # can probably get rid of this it only really makes sense if planning on subtracting the score, another way would be to only count button presses on push down instead of push up 
-            elif any([BUTTON.value() for BUTTON in BUTTONS]):
-                # score -= 1
-                print(score) 
-                # sleep(0.1) # can probably get rid of this it only really makes sense if planning on subtracting the score, another way would be to only count button presses on push down instead of push up
+        for PLAYER in PLAYERS:
+            SELECTED = PLAYER["SELECTED"]
+            LEDS = PLAYER["LEDS"]
+            updateLeds(LEDS, SELECTED)
+            for i, num in enumerate(SELECTED):
+                if PLAYER["BUTTONS"][num].value():
+                    score_needs_update = True
+                    PLAYER["SCORE"] += 1
+                    SELECTED[i] = getRandomInt(0, len(LEDS), SELECTED)
         
-        for i, num in enumerate(selected2):
-            if BUTTONS2[num].value():
-                score2 += 1
-                selected2[i] = getRandomInt(0, len(LEDS2), selected2)
-                print(score2)
-                # sleep(0.1) # can probably get rid of this it only really makes sense if planning on subtracting the score, another way would be to only count button presses on push down instead of push up 
-            elif any([BUTTON.value() for BUTTON in BUTTONS2]):
-                # score2 -= 1
-                print(score2) 
-                # sleep(0.1) # can probably get rid of this it only really makes sense if planning on subtracting the score, another way would be to only count button presses on push down instead of push up
-            
-            
-        if startScore != score or startScore2 != score2 or next_update_time <= curr_time:
-            lcd.clear() 
-            lcd.putstr("P1 SCORE: " + str(score)+"\n")
-            lcd.putstr("P2 SCORE: "+str(score2)+"\n")
-            lcd.putstr("TIME REMAINING: "+str((start_time + GAME_LENGTH - curr_time) // 1))
+        if score_needs_update or next_update_time <= curr_time:
+            lcd.clear()
+            displayPlayerScores(lcd, PLAYERS)
+            lcd.putstr("TIME REMAINING: " + str((start_time + GAME_DURATION - curr_time) // 1) + "\n")
             next_update_time = (curr_time + 1) // 1
+            score_needs_update = False
 
-            
-        if start_time + GAME_LENGTH <= curr_time:
+        if start_time + GAME_DURATION <= curr_time:
             started = False
             lcd.clear()
-            lcd.putstr("P1 SCORE: " + str(score)+"\n")
-            lcd.putstr("P2 SCORE: "+str(score2)+"\n")
-            if score == score2:
-                lcd.putstr("TIE!!!")
-            elif score > score2:
-                lcd.putstr("P1 WINS!!!")
-            else :
-                lcd.putstr("P2 WINS!!!")
+            displayPlayerScores(lcd, PLAYERS)
+            winners = displayAndReturnWinners(lcd, PLAYERS)
+            turnOffAllLeds(PLAYERS)
+
+    # flashing effect for winning players
+    if winners is not None and next_flash_time <= curr_time:
+        if is_flash_on:
+            turnOffAllLeds(PLAYERS)
+        else:
+            for winner in winners:
+                turnOnAllPlayerLeds(winner)
+        is_flash_on = not is_flash_on
+        next_flash_time = curr_time + WINNER_FLASH_DURATION
         
+
     if START_BUTTON.value():
-        score = 0
-        score2 = 0
-        selected = getRandomIntArray(0, len(LEDS), NUM_SELECTED)
-        selected2 = getRandomIntArray(0, len(LEDS2), NUM_SELECTED)
-        manageLights(LEDS, [])
-        manageLights(LEDS2, [])
-        for i in range(COUNTDOWN_TIME, 0, -1):
+        for i in range(COUNTDOWN_DURATION, 0, -1):
             lcd.clear()
-            lcd.putstr("STARTING IN: " + str(i) + "!")
+            lcd.putstr("{:^20}".format("STARTING IN: " + str(i) + "!"))
             sleep(1)
+        resetPlayers(PLAYERS, NUM_SELECTED)
+        winner = None
         started = True
         curr_time = time()
         start_time = curr_time
-        next_update_time = curr_time + 1 // 1
-        lcd.clear() 
-        lcd.putstr("P1 SCORE: " + str(score)+"\n")
-        lcd.putstr("P2 SCORE: "+str(score2)+"\n")
-        lcd.putstr("TIME REMAINING: "+str((start_time + GAME_LENGTH - curr_time) // 1))
+        next_update_time = curr_time
+        lcd.putstr("\n") # No idea why but if this isn't here the first print on line 74 shows on 1 line instead of being split with the new lines
 
--- PI_PICO/utils.py
+++ PI_PICO/utils.py
@@ -10,6 +10,33 @@ def getRandomIntArray(start, end, length):
         arr[i] = getRandomInt(start, end, arr)
     return arr
 
-def manageLights(LEDS, selected):
+def updateLeds(LEDS, selected):
     for i, LED in enumerate(LEDS):
         LED.value(i in selected)
+        
+def turnOffAllLeds(PLAYERS):
+    for PLAYER in PLAYERS:
+        updateLeds(PLAYER["LEDS"], [])
+        
+def turnOnAllPlayerLeds(PLAYER):
+    updateLeds(PLAYER["LEDS"], getRandomIntArray(0, len(PLAYER["LEDS"]), len(PLAYER["LEDS"])))
+        
+def resetPlayers(PLAYERS, NUM_SELECTED):
+    for PLAYER in PLAYERS:
+        PLAYER["SCORE"] = 0
+        PLAYER["SELECTED"] = getRandomIntArray(0, len(PLAYER["LEDS"]), NUM_SELECTED)
+        updateLeds(PLAYER["LEDS"], [])
+
+def displayPlayerScores(lcd, PLAYERS):
+    for PLAYER in PLAYERS:
+        lcd.putstr(PLAYER["NAME"] + " SCORE: " + str(PLAYER["SCORE"]) + "\n")
+        
+def displayAndReturnWinners(lcd, PLAYERS):
+    # sort desc
+    sortedPlayers = sorted(PLAYERS, key = lambda PLAYER : -PLAYER["SCORE"]) 
+    if sortedPlayers[0]["SCORE"] == sortedPlayers[1]["SCORE"]:
+        lcd.putstr("{:^20}".format("TIE!!!"))
+    else:
+        lcd.putstr("{:^20}".format(sortedPlayers[0]["NAME"] + " WINS!!!"))
+        
+    return [PLAYER for PLAYER in sortedPlayers if PLAYER["SCORE"] == sortedPlayers[0]["SCORE"]]
